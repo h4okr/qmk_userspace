54ae9401226bc61745b7f6a8022d19f3a259b2cf
Arnaud Crohen <133008493+h4okr@users.noreply.github.com>
2026-02-02 01:07:24 +0100

    duplication of stdev-nobl for rp2040

---
+++ keyboards/ymdk/id75/rp2040/keymaps/aokb-pseudosplit-stdev-nobl/rules.mk
@@ -0,0 +1,14 @@
+# additional source files
+
+# override default userspace
+USER_NAME := h4okr/stdev-nobl
+
+# features
+CAPS_WORD_ENABLE = yes
+LAYER_LOCK_ENABLE = yes # use QK_LLCK
+EXTRAKEY_ENABLE = yes
+SEND_STRING_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+KEY_LOCK_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+NKRO_ENABLE = yes
