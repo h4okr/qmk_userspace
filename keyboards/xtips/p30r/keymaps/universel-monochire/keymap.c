9006a2583e261015b98f79dd87442515b3a2d7ae
Arnaud Crohen <133008493+h4okr@users.noreply.github.com>
2026-03-22 02:22:29 +0100

    leds.c is useless, no led control on p30r

--- keyboards/xtips/p30r/keymaps/universel-monochire/keymap.c
+++ keyboards/xtips/p30r/keymaps/universel-monochire/keymap.c
@@ -1,5 +1,5 @@
 #include QMK_KEYBOARD_H
-#include "./leds.c"
+//#include "./leds.c"
 #include "./layers.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_FUNCTION] = LAYOUT( 
 		KC_F1,	KC_F2,	KC_F3,	KC_F4,	KC_F5,  KC_F6,
 		KC_F7,	KC_F8,	KC_F9,	KC_F10,	KC_F11, KC_F12,
-		KC_LGUI,BL_TOGG,BL_DOWN,BL_UP,  _______,_______,
+		KC_LGUI,_______,_______,_______,_______,_______,
 		_______,_______,_______,_______,_______,_______, 
 		_______,_______,_______,MN_NUM, MN_CONF,KC_PSCR), 
 
